7a9af7517c9c4b9f40a30873418a0f9e7a4e7995
Husni Sabah <sano84@yahoo.com>
2022-09-10 06:23:30 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+// detecting button press
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    /// change console.log(this.imageHTML to white color)
+    ///this.style.color = "white";
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+
+      //  audio to name of instrument
+
+      // copy and paste swich in makeSound function
+
+  });
+
+}
+
+// detecting keyboard press
+// keydown instead of keypress
+
+
+document.addEventListener("keydown", function(event) {
+  // remove console.log(event);
+  makeSound(event.key);
+  buttonAnimation(event.key);
+
+});
+
+  // buttonInnerHTML to key
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+    }
+  };
+// adding animation
+// list of classes classList
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout (function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
